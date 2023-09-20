37534b5a3f694b215efe1e7f123652e9d9febe76
Максим Портнов <maximportnov99@yandex.ru>
2023-09-20 21:32:59 +0300

    delete line djoser

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -111,7 +111,6 @@ REST_FRAMEWORK = {
 
 DJOSER = {
     "SERIALIZERS": {
-        "user_create": "api.serializers.UserCreateSerializer",
         "user": "api.serializers.UserSerializer",
         "current_user": "api.serializers.UserSerializer",
     },
