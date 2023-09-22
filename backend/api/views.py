a021348c7f809039f28cdae30a88c21dbd33f858
Максим Портнов <maximportnov99@yandex.ru>
2023-09-22 11:13:52 +0300

    rename amount

--- backend/api/views.py
+++ backend/api/views.py
@@ -73,7 +73,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
             recipe__shopping_list__user=request.user
         ).order_by('ingredient__name').values(
             'ingredient__name', 'ingredient__measurement_unit'
-        ).annotate(cart_amount=Sum('amount'))
+        ).annotate(amount=Sum('amount'))
         return self.send_message(ingredients)
 
     @action(
