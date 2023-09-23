8c492ad708aa0791e529e28952901dabca6379c6
Максим Портнов <maximportnov99@yandex.ru>
2023-09-23 11:36:50 +0300

    total_amount

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Sum, PositiveSmallIntegerField
+from django.db.models import PositiveSmallIntegerField, Sum
 from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
